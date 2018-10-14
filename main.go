c03d4e014d7b3a4816f0c54281d1f5859af60ff8
gara45 <h-yoda@asobism.co.jp>
2018-10-14 16:05:53 +0900

    Initial Commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+// BigHuge is
+type BigHuge struct {
+	APIKey string
+}
+
+type synonyms struct {
+	Noun *words `json:"noun"`
+	Verb *words `json:"verb"`
+}
+
+type words struct {
+	Syn []string `json:"syn"`
+}
+
+func main() {
+
+}
